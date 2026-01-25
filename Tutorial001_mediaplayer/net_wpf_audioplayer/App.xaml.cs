fd22364c27f2482c11b27cb38d84452c29d7e10a
cyberkdh <cyberkdh@gmail.com>
2026-01-25 15:20:29 +0900

    net_wpf_audioplayer ..

---
+++ Tutorial001_mediaplayer/net_wpf_audioplayer/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////
+//	Projects		: Tutorial for mediaplay
+//	Author			: CyberKDH(cyberkdh@hotmail.com, cyberkdh@gmail.com)
+//	Module			: 
+//	History			: 
+//	Copyrights		: Copyright ⓒCYBERKDH. All Rights Reserved.
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+using FFmpeg.AutoGen;
+using net_wpf_audioplayer.module;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace net_wpf_audioplayer {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        private void Application_Startup(object sender, StartupEventArgs e) {            
+            // assume the ffmpeg dlls are in the x64 subfolder of the exe module
+            ffmpeg.RootPath = Path.Combine(AppDefine.Get().GetPGMBasePath(), "ffmpeg_x64");
+		}
+	}
+
+}
---
+++ Tutorial001_mediaplayer/net_wpf_audioplayer/module/UtilsCls.cs
@@ -0,0 +1,61 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////
+//	Projects		: Tutorial for mediaplay
+//	Author			: CyberKDH(cyberkdh@hotmail.com, cyberkdh@gmail.com)
+//	Module			: 
+//	History			: 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace net_wpf_audioplayer.module {
+	public class TSVar<T> {
+
+		private object m_objlock = new object();
+		private T? m_value;
+
+		public T Value {
+			get {
+				T retval;
+				lock (m_objlock) {
+					retval = m_value;
+				}
+				return retval;
+			}
+			set {
+				lock (m_objlock) {
+					m_value = value;
+				}
+			}
+		}
+
+		public TSVar(T val) {
+			Value = val;
+		}
+
+	}
+
+	public class SecsToTimeConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			var ts = TimeSpan.FromSeconds((double)value);
+
+			string strret = "";
+
+			if (ts.TotalHours > 9) {
+				strret = ((int)ts.TotalHours) + ts.ToString(@"\:mm\:ss");
+			}
+			else {
+				strret = ts.ToString(@"hh\:mm\:ss");
+			}
+
+			//Debug.WriteLine($"= [SecsToTimeConverter] Convert: {strret}");
+			return (string)strret;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
